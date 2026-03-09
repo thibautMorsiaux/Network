9a3a2e01e78f5e2c13021193ac39b7df92d23b8a
Thibaut Morsiaux <thibautmorsiaux@gmail.com>
2026-03-09 18:02:10 +0100

    server ecoute ! to do : - link simulator - changer de méthode pour passer de GO BACK N à SACK

--- src/server.py
+++ src/server.py
@@ -17,6 +17,16 @@ MAX_LENGTH = 1024
 
 DEFAULT_DIRECTORY = '.'
 
+
+#pour lire les ack
+def decode_ack(raw: bytes):
+    if len(raw) < 12:
+        return None, None
+    word = int.from_bytes(raw[0:4], byteorder='big')
+    ptype = (word >> 30) & 0x3
+    seqnum = word & 0x7ff
+    return ptype, seqnum
+
 def create_server(server_addr: str, port: int, directory: str):
     try:
         sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
@@ -49,16 +59,55 @@ def create_server(server_addr: str, port: int, directory: str):
                 seq_payloads = [payload[i:i+1024] for i in range(0, len(payload), 1024)]
             else:
                 seq_payloads = [payload]
+            # dernier seg vide ajouté pour qu'il soit géré par la window coulissante
+            seq_payloads.append(b"")
 
-            for elements in seq_payloads:
-                segment = encode(type_str="PTYPE_DATA", window=0, seqnum=seqnum, timestamp=timestamp, payload=elements)
-                sock.sendto(segment, client_addr)
-                print(f"Segment DATA envoyé ({len(elements)} bytes)", file=sys.stderr)
-                seqnum += 1
-            sock.sendto(encode(type_str="PTYPE_DATA", window=0, seqnum=seqnum, timestamp=timestamp, payload=b""), client_addr) #Envoi du dernier bit qui indique la fin au client.
-            print(f"Dernier segment DATA envoyé (0 bytes)", file=sys.stderr)
-            print(f"Fichier envoyé pour un total de {len(payload)} bits")
-        print("out of while True loop")
+            total_chunks = len(seq_payloads)
+            base = 0
+            next_to_send = 0
+            window_size = 10 
+            sock.settimeout(0.5) #attendre les acks
+
+            while base < total_chunks:
+                # envoi de paquets (dans la limite de la fenetre)
+                while next_to_send < base + window_size and next_to_send < total_chunks:
+                    elements = seq_payloads[next_to_send]
+                    current_seqnum = next_to_send % 2048
+                    
+                    segment = encode(type_str="PTYPE_DATA", window=0, seqnum=current_seqnum, timestamp=timestamp, payload=elements)
+                    sock.sendto(segment, client_addr)
+                    
+                    if not elements:
+                        print(f"Dernier segment DATA envoyé (Seq: {current_seqnum}, 0 bytes)", file=sys.stderr)
+                    else:
+                        print(f"Segment DATA envoyé (Seq: {current_seqnum}, {len(elements)} bytes)", file=sys.stderr)
+                    
+                    next_to_send += 1
+
+                #écoute des ack
+                try:
+                    raw_ack, _ = sock.recvfrom(2048)
+                    ptype, ack_seqnum = decode_ack(raw_ack)
+                    if ptype == PTYPE_ACK:
+                        print(f"ACK reçu -> le client attend le seq {ack_seqnum}", file=sys.stderr)
+                        #avance la base
+                        expected_base_seq = base % 2048
+                        if ack_seqnum > expected_base_seq:
+                            base += (ack_seqnum - expected_base_seq)
+                        elif ack_seqnum < expected_base_seq:
+                            base += (2048 - expected_base_seq) + ack_seqnum
+                            
+                except socket.timeout:
+                    #si on recoit rien apres 0.5s, on recule et on renvoie
+                    print("Timeout ! Aucun ACK reçu. On renvoie la fenetre.", file=sys.stderr)
+                    next_to_send = base
+            
+            
+            sock.settimeout(None) #pour ecouter le prochain client
+            print(f"Fichier envoyé pour un total de {len(payload)} bytes")
+
+        else:
+            print("Request is not in the valid format.")
     except socket.error as err:
         print(f'Erreur socket: {err}', file=sys.stderr)
         return -1
@@ -121,3 +170,4 @@ if __name__ == "__main__":
     
     if result != 0:
         sys.exit(1)
+
